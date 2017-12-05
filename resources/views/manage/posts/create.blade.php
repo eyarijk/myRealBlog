397cabcf387fd1bf186c32938741c87055f36222
eyarijk <eyarij@gmail.com>
2017-12-06 00:44:54 +0200

    Slug in created post

--- resources/views/manage/posts/create.blade.php
+++ resources/views/manage/posts/create.blade.php
@@ -15,20 +15,21 @@
     <form action="{{route('posts.store')}}" method="post">
       {{ csrf_field() }}
       <div class="columns">
-        <div class="column is-three-quarters-desktop is-three-quarters-tablet">
+        <div class="column is-three-quarters-desktop">
           <b-field>
-            <b-input type="text" placeholder="Post Title" size="is-large" >
+            <b-input type="text" placeholder="Post Title" size="is-large" v-model="title">
             </b-input>
           </b-field>
 
+          <slug-widget url="{{url('/')}}" subdirectory="blog" :title="title" @slug-changed="updateSlug"></slug-widget>
+          <input type="hidden" v-model="slug" name="slug" />
 
           <b-field class="m-t-40">
             <b-input type="textarea"
                 placeholder="Compose your masterpiece..." rows="20">
             </b-input>
           </b-field>
-        </div>
-
+        </div> <!-- end of .column.is-three-quarters -->
         <div class="column is-one-quarter-desktop is-narrow-tablet">
           <div class="card card-widget">
             <div class="author-widget widget-area">
@@ -75,7 +76,16 @@
   <script>
     var app = new Vue({
       el: '#app',
-      data: {}
+      data: {
+        title: '',
+        slug: '',
+        api_token: '{{Auth::user()->api_token}}'
+      },
+      methods: {
+        updateSlug: function(val) {
+          this.slug = val;
+        }
+      }
     });
   </script>
 @endsection
